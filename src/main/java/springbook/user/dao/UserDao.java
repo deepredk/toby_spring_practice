605db7e605adc1483650ceeb9535d7340bb5d370
deepredk <deepred.kim@gmail.com>
2022-06-06 19:34:28 +0900

    Initial commit

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,15 @@
+package springbook.user.dao;
+
+public class UserDao {
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+}
