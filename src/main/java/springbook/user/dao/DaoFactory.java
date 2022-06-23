713844607e06e0e16ce93231a17c4fb6eebdd244
deepredk <deepred.kim@gmail.com>
2022-06-23 20:32:25 +0900

    ~1.8.4 DataSource로 전환

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,13 +1,24 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
 public class DaoFactory {
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
     }
 
-    public ConnectionMaker connectionMaker() {
-        return new SimpleConnectionMaker();
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/springbook?useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+
+        return dataSource;
     }
 }
