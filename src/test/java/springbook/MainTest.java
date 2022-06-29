4d0014c9659d280187e4a053b55a90e2457698cf
Deepred <deepred.kim@gmail.com>
2022-06-29 13:48:08 +0900

    ~2.2.2 테스트의 효율적인 수행과 결과 관리

--- src/test/java/springbook/MainTest.java
+++
@@ -1,5 +0,0 @@
-package springbook;
-
-public class MainTest {
-    
-}
---
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,35 @@
+package springbook.user.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+
+		User user = new User();
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
+
+		dao.add(user);
+
+		User user2 = dao.get(user.getId());
+        assertEquals(user.getName(), user2.getName());
+        assertEquals(user.getPassword(), user2.getPassword());
+
+		((ConfigurableApplicationContext) context).close();
+	}
+
+}
