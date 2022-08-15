bd9a090e65edb30f1fca6f666c3719dd907bafc7
deepredk <deepred.kim@gmail.com>
2022-08-15 23:14:44 +0900

    ~6.5.3 포인트컷 표현식을 이용한 포인트컷

---
+++ src/test/java/springbook/learningtest/pointcut/Target.java
@@ -0,0 +1,30 @@
+package springbook.learningtest.pointcut;
+
+public class Target implements TargetInterface {
+
+    @Override
+    public void hello() {
+        
+    }
+
+    @Override
+    public void hello(String a) {
+        
+    }
+
+    @Override
+    public void method() {
+        
+    }
+
+    @Override
+    public int minus(int a, int b) throws RuntimeException {
+        return 0;
+    }
+
+    @Override
+    public int plus(int a, int b) {
+        return 0;
+    }
+    
+}
