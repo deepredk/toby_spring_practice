abba6ba05937ff45c88729f6c5ea8f1e53e85e5e
deepredk <deepred.kim@gmail.com>
2022-09-10 21:52:17 +0900

    ~7.5.3 트랜잭션 적용

--- src/test/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/test/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -3,9 +3,14 @@ package springbook.user.sqlservice.updatable;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
+import static org.junit.Assert.fail;
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
+import org.junit.Test;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
     
@@ -25,6 +30,22 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
     @After
     public void tearDown() {
         db.shutdown();
+    }
+
+    @Test
+    public void transactionalUpdate() {
+        checkFind("SQL1", "SQL2", "SQL3");
+        
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY9999!@#$", "Modified9999");
+
+        try {
+            sqlRegistry.updateSql(sqlmap);
+            fail();
+        } catch (SqlUpdateFailureException e) { }
+
+        checkFind("SQL1", "SQL2", "SQL3");
     }
 
 }
